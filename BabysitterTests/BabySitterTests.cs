c4888074399bc6527cfb1a64651e5baf439c657f
Hilary Sasso-Schleh <hsassoschleh@gmail.com>
2019-01-16 10:33:07 -0500

    babysitter

---
+++ BabysitterTests/BabySitterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Babysitter_Kata;
+
+namespace BabysitterTests
+{
+    [TestClass]
+    public class BabySitterTests
+    {
+        [TestMethod]
+        public void WhenIsAllowableStartTimePassedDateTimeReturnsFalse()
+        {
+            Job job = new Job();
+
+
+        }
+    }
+}
---
+++ Babysitter_Kata/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babysitter_Kata
+{
+    public class Job
+    {
+        public Job(DateTime StartTime)
+        {
+
+        }
+    }
+}
